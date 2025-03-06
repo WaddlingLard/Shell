f1d5ffba797f593ecedca4b53c580746d7313e86
Brian <dathamor@gmail.com>
2025-03-06 16:43:35 -0700

    deq hotfix

--- hw3/deq.c
+++ hw3/deq.c
@@ -6,35 +6,42 @@
 #include "error.h"
 
 // indices and size of array of node pointers
-typedef enum {
+typedef enum
+{
   Head,
   Tail,
   Ends,
 
   Next = Head, // 0
-  Prev = Tail // 1
+  Prev = Tail  // 1
 
 } End;
 
-typedef struct Node {
-  struct Node *np[Ends];        // next/prev neighbors
+typedef struct Node
+{
+  struct Node *np[Ends]; // next/prev neighbors
   Data data;
 } *Node;
 
-typedef struct {
-  Node ht[Ends];                // head/tail nodes
+typedef struct
+{
+  Node ht[Ends]; // head/tail nodes
   int len;
 } *Rep;
 
-static Rep rep(Deq q) {
-  if (!q) ERROR("zero pointer");
+static Rep rep(Deq q)
+{
+  if (!q)
+    ERROR("zero pointer");
   return (Rep)q;
 }
 
-static void put(Rep r, End e, Data d) {
-  
+static void put(Rep r, End e, Data d)
+{
+
   // Validating data
-  if (!d) ERROR("DATA ERROR");
+  if (!d)
+    ERROR("DATA ERROR");
 
   // Setting directional flag, ex: if e is the head (0) then the direction 1 for next
   int dirE = (!e) ? Prev : Next;
@@ -43,11 +50,13 @@ static void put(Rep r, End e, Data d) {
   Node add = (Node)malloc(sizeof(struct Node));
 
   // Validating new node
-  if (!add) ERROR("malloc() failed");
+  if (!add)
+    ERROR("malloc() failed");
   add->data = d;
 
   // e can either be Head = 0, or Tail = 1
-  if (r->len != 0) {
+  if (r->len != 0)
+  {
     // Grab the node, either at the tail or head
     Node end = r->ht[e];
 
@@ -59,8 +68,9 @@ static void put(Rep r, End e, Data d) {
 
     // Readjust where the end pointer is
     r->ht[e] = add;
-
-  } else {
+  }
+  else
+  {
     // No data, head and tail will point to same node
     r->ht[Head] = add;
     r->ht[Tail] = add;
@@ -70,10 +80,12 @@ static void put(Rep r, End e, Data d) {
   r->len = r->len + 1;
 }
 
-static Data ith(Rep r, End e, int i)  { 
+static Data ith(Rep r, End e, int i)
+{
 
   // Checking to see if ith is a valid call
-  if (r->len == 0 || i > r->len) ERROR("Invalid method call!");
+  if (r->len == 0 || i > r->len)
+    ERROR("Invalid method call!");
 
   // Setting directional flag, ex: if e is the head (0) then the direction 1 for next
   int dirE = (!e) ? Prev : Next;
@@ -81,10 +93,11 @@ static Data ith(Rep r, End e, int i)  {
   // Grabbing the current node and data to iterate
   Node curr = r->ht[e];
   Data pulled = curr->data;
-  
+
   // Using 0-based index and iterate over each node until 1 == index
   int index = 0;
-  while (i != index++) {
+  while (i != index++)
+  {
     // Grab the next element and update the current data
     curr = curr->np[dirE];
     pulled = curr->data;
@@ -94,27 +107,32 @@ static Data ith(Rep r, End e, int i)  {
   return pulled;
 }
 
-static Data get(Rep r, End e)         { 
+static Data get(Rep r, End e)
+{
 
   // Invalid call, nothing in the deq
-  if (r->len == 0) ERROR("no items in deq");
+  if (r->len == 0)
+    ERROR("no items in deq");
 
   // Setting directional flag, ex: if e is the head (0) then the direction 1 for next
   int dirE = (!e) ? Prev : Next;
-  
+
   // Grabbing node and pulling data
   Node end = r->ht[e];
-  Data pulled = end->data;  
+  Data pulled = end->data;
 
   // deq is empty now
-  if (r->len - 1 == 0) {
+  if (r->len - 1 == 0)
+  {
     // Setting both ends to null as there is nothing in the deq
     r->ht[e] = NULL;
     r->ht[dirE] = NULL;
 
     // Freeing grabbed node
     free(end);
-  } else {
+  }
+  else
+  {
     // Readjusting pointers
 
     // Grabbing the node which is adjacent to the grabbed one
@@ -125,7 +143,7 @@ static Data get(Rep r, End e)         {
 
     // Adjusting end pointer
     r->ht[e] = adjacent;
-    
+
     // Freeing grabbed node
     free(end);
   }
@@ -136,26 +154,31 @@ static Data get(Rep r, End e)         {
   return pulled;
 }
 
-static Data rem(Rep r, End e, Data d) { 
+static Data rem(Rep r, End e, Data d)
+{
 
   // Invalid call, nothing in the deq
-  if (r->len == 0) ERROR("No items in deq");
+  if (r->len == 0)
+    ERROR("No items in deq");
 
   // Setting directional flag, ex: if e is the head (0) then the direction 1 for next
   int dirE = (!e) ? Prev : Next;
-  
+
   // Grabbing node and pulling data
   Node current = r->ht[e];
   Data pulled = current->data;
 
   // Used to store the node that will be removed
   Node remove;
-  
+
   // Loop through to find the matching data
   int index = 0;
-  while (index != r->len) {
-    // Have to use '==' comparison
-    if (pulled == d) {
+  while (index != r->len)
+  {
+
+    // Have to use '==' comparison on dereferenced values
+    if (*(char *)pulled == *(char *)d)
+    {
       remove = current;
       // printf("Found a match!");
       break;
@@ -163,7 +186,16 @@ static Data rem(Rep r, End e, Data d) {
 
     // Refresh values to continue looking
     current = current->np[dirE];
-    pulled = current->data;
+
+    // Final check, fails if match not found
+    if (!current)
+    {
+      ERROR("Data is not in the list");
+    }
+    else
+    {
+      pulled = current->data;
+    }
 
     // Incrementing index
     index++;
@@ -171,18 +203,18 @@ static Data rem(Rep r, End e, Data d) {
     // After executing, the loop either found a match, or it reached the end
   }
 
-  // Final check, fails if match not found
-  if (pulled != d) ERROR("Data is not in the list");
-
   // deq is empty now
-  if (r->len - 1 == 0) {
+  if (r->len - 1 == 0)
+  {
     // Setting both ends to null as there is nothing in the deq
     r->ht[e] = NULL;
     r->ht[dirE] = NULL;
 
     // Freeing grabbed node
     free(remove);
-  } else if (index == 0){
+  }
+  else if (index == 0)
+  {
     // At the front of the searching point
 
     // Grabbing the node which is adjacent to the grabbed one
@@ -193,12 +225,14 @@ static Data rem(Rep r, End e, Data d) {
 
     // Adjusting end pointer
     r->ht[e] = adjacent;
-    
+
     // Freeing grabbed node
     free(remove);
-  } else if (index == r->len - 1) {
+  }
+  else if (index == r->len - 1)
+  {
     // At the end of the searching point
-    
+
     // Grabbing the node which is adjacent to the grabbed one
     Node adjacent = current->np[e];
 
@@ -206,14 +240,16 @@ static Data rem(Rep r, End e, Data d) {
     adjacent->np[dirE] = NULL;
 
     // Adjusting end pointer
-    r->ht[dirE] = adjacent; 
-    
+    r->ht[dirE] = adjacent;
+
     // Freeing grabbed node
     free(remove);
-  } else {
+  }
+  else
+  {
     // Somewhere in the middle
     // printf("%d %d", index, r->len);
-    
+
     // Going to need to grab 2 nodes, one ahead of the grabbed and one before
     Node adjacentForward, adjacentBackward;
     adjacentForward = current->np[dirE];
@@ -224,7 +260,7 @@ static Data rem(Rep r, End e, Data d) {
 
     // The node behind of the grabbed node will be attached to the next one
     adjacentBackward->np[dirE] = adjacentForward;
-    
+
     // Freeing grabbed node
     free(remove);
   }
@@ -232,53 +268,64 @@ static Data rem(Rep r, End e, Data d) {
   // Decrementing size
   r->len = r->len - 1;
 
-  return pulled; 
+  return pulled;
 }
 
-extern Deq deq_new() {
-  Rep r=(Rep)malloc(sizeof(*r));
-  if (!r) ERROR("malloc() failed");
-  r->ht[Head]=0;
-  r->ht[Tail]=0;
-  r->len=0;
+extern Deq deq_new()
+{
+  Rep r = (Rep)malloc(sizeof(*r));
+  if (!r)
+    ERROR("malloc() failed");
+  r->ht[Head] = 0;
+  r->ht[Tail] = 0;
+  r->len = 0;
   return r;
 }
 
 extern int deq_len(Deq q) { return rep(q)->len; }
 
-extern void deq_head_put(Deq q, Data d) {        put(rep(q),Head,d); }
-extern Data deq_head_get(Deq q)         { return get(rep(q),Head);   }
-extern Data deq_head_ith(Deq q, int i)  { return ith(rep(q),Head,i); }
-extern Data deq_head_rem(Deq q, Data d) { return rem(rep(q),Head,d); }
+extern void deq_head_put(Deq q, Data d) { put(rep(q), Head, d); }
+extern Data deq_head_get(Deq q) { return get(rep(q), Head); }
+extern Data deq_head_ith(Deq q, int i) { return ith(rep(q), Head, i); }
+extern Data deq_head_rem(Deq q, Data d) { return rem(rep(q), Head, d); }
 
-extern void deq_tail_put(Deq q, Data d) {        put(rep(q),Tail,d); }
-extern Data deq_tail_get(Deq q)         { return get(rep(q),Tail);   }
-extern Data deq_tail_ith(Deq q, int i)  { return ith(rep(q),Tail,i); }
-extern Data deq_tail_rem(Deq q, Data d) { return rem(rep(q),Tail,d); }
+extern void deq_tail_put(Deq q, Data d) { put(rep(q), Tail, d); }
+extern Data deq_tail_get(Deq q) { return get(rep(q), Tail); }
+extern Data deq_tail_ith(Deq q, int i) { return ith(rep(q), Tail, i); }
+extern Data deq_tail_rem(Deq q, Data d) { return rem(rep(q), Tail, d); }
 
-extern void deq_map(Deq q, DeqMapF f) {
-  for (Node n=rep(q)->ht[Head]; n; n=n->np[Tail])
+extern void deq_map(Deq q, DeqMapF f)
+{
+  for (Node n = rep(q)->ht[Head]; n; n = n->np[Tail])
     f(n->data);
 }
 
-extern void deq_del(Deq q, DeqMapF f) {
-  if (f) deq_map(q,f);
-  Node curr=rep(q)->ht[Head];
-  while (curr) {
-    Node next=curr->np[Tail];
+extern void deq_del(Deq q, DeqMapF f)
+{
+  if (f)
+    deq_map(q, f);
+  Node curr = rep(q)->ht[Head];
+  while (curr)
+  {
+    Node next = curr->np[Tail];
     free(curr);
-    curr=next;
+    curr = next;
   }
   free(q);
 }
 
-extern Str deq_str(Deq q, DeqStrF f) {
-  char *s=strdup("");
-  for (Node n=rep(q)->ht[Head]; n; n=n->np[Tail]) {
-    char *d=f ? f(n->data) : n->data;
-    char *t; asprintf(&t,"%s%s%s",s,(*s ? " " : ""),d);
-    free(s); s=t;
-    if (f) free(d);
+extern Str deq_str(Deq q, DeqStrF f)
+{
+  char *s = strdup("");
+  for (Node n = rep(q)->ht[Head]; n; n = n->np[Tail])
+  {
+    char *d = f ? f(n->data) : n->data;
+    char *t;
+    asprintf(&t, "%s%s%s", s, (*s ? " " : ""), d);
+    free(s);
+    s = t;
+    if (f)
+      free(d);
   }
   return s;
 }
