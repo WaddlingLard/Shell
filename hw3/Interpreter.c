d530631640dea8c65cabc0172fdbec5312149249
brianwu <dathamor@gmail.com>
2025-03-25 01:01:19 -0600

    modified for redir

--- hw3/Interpreter.c
+++ hw3/Interpreter.c
@@ -11,35 +11,39 @@ static Command i_command(T_command t);
 static void i_pipeline(T_pipeline t, Pipeline pipeline);
 static void i_sequence(T_sequence t, Sequence sequence);
 
-static Command i_command(T_command t) {
+static Command i_command(T_command t)
+{
   if (!t)
     return 0;
-  Command command=0;
+  Command command = 0;
   if (t->words)
-    command=newCommand(t->words);
+    command = newCommand(t);
   return command;
 }
 
-static void i_pipeline(T_pipeline t, Pipeline pipeline) {
+static void i_pipeline(T_pipeline t, Pipeline pipeline)
+{
   if (!t)
     return;
-  addPipeline(pipeline,i_command(t->command));
-  i_pipeline(t->pipeline,pipeline);
+  addPipeline(pipeline, i_command(t->command));
+  i_pipeline(t->pipeline, pipeline);
 }
 
-static void i_sequence(T_sequence t, Sequence sequence) {
+static void i_sequence(T_sequence t, Sequence sequence)
+{
   if (!t)
     return;
-  Pipeline pipeline=newPipeline(1);
-  i_pipeline(t->pipeline,pipeline);
-  addSequence(sequence,pipeline);
-  i_sequence(t->sequence,sequence);
+  Pipeline pipeline = newPipeline(1);
+  i_pipeline(t->pipeline, pipeline);
+  addSequence(sequence, pipeline);
+  i_sequence(t->sequence, sequence);
 }
 
-extern void interpretTree(Tree t, int *eof, Jobs jobs) {
+extern void interpretTree(Tree t, int *eof, Jobs jobs)
+{
   if (!t)
     return;
-  Sequence sequence=newSequence();
-  i_sequence(t,sequence);
-  execSequence(sequence,jobs,eof);
+  Sequence sequence = newSequence();
+  i_sequence(t, sequence);
+  execSequence(sequence, jobs, eof);
 }
