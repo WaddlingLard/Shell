3204b59577a7cd2a5a16a2d35e452112550ceec6
brianwu <dathamor@gmail.com>
2025-03-25 17:42:35 -0600

    implemented a getter for last process and handles background jobs, modified struct too

--- hw3/Pipeline.c
+++ hw3/Pipeline.c
@@ -8,49 +8,69 @@
 #include "deq.h"
 #include "error.h"
 
-typedef struct {
+typedef struct
+{
   Deq processes;
-  int fg;			// not "&"
+  int fg; // not "&"
+  int lastprocessid;
 } *PipelineRep;
 
-extern Pipeline newPipeline(int fg) {
-  PipelineRep r=(PipelineRep)malloc(sizeof(*r));
+extern Pipeline newPipeline(int fg)
+{
+  PipelineRep r = (PipelineRep)malloc(sizeof(*r));
   if (!r)
     ERROR("malloc() failed");
-  r->processes=deq_new();
-  r->fg=fg;
+  r->processes = deq_new();
+  r->fg = fg;
   return r;
 }
 
-extern void addPipeline(Pipeline pipeline, Command command) {
-  PipelineRep r=(PipelineRep)pipeline;
-  deq_tail_put(r->processes,command);
+extern void addPipeline(Pipeline pipeline, Command command)
+{
+  PipelineRep r = (PipelineRep)pipeline;
+  deq_tail_put(r->processes, command);
 }
 
-extern int sizePipeline(Pipeline pipeline) {
-  PipelineRep r=(PipelineRep)pipeline;
+extern int sizePipeline(Pipeline pipeline)
+{
+  PipelineRep r = (PipelineRep)pipeline;
   return deq_len(r->processes);
 }
 
-static void execute(Pipeline pipeline, Jobs jobs, int *jobbed, int *eof) {
+static void execute(Pipeline pipeline, Jobs jobs, int *jobbed, int *eof)
+{
 
-  PipelineRep r=(PipelineRep)pipeline;
+  PipelineRep r = (PipelineRep)pipeline;
+  Command last = deq_tail_ith(r->processes, 0);
+  // fprintf(stdout, "Executing pipeline... , number of jobs: %d\n", sizePipeline(r));
 
-  fprintf(stdout, "Executing pipeline... , number of jobs: %d\n", sizePipeline(r));
-  
-  for (int i=0; i<sizePipeline(r) && !*eof; i++)
-    execCommand(deq_head_ith(r->processes,i),pipeline,jobs,jobbed,eof,1);
+  for (int i = 0; i < sizePipeline(r) && !*eof; i++)
+    execCommand(deq_head_ith(r->processes, i), pipeline, jobs, jobbed, eof, r->fg);
+
+  r->lastprocessid = getProcessID(last);
+  if (r->fg)
+  {
+    wait(r->lastprocessid);
+  }
 }
 
-extern void execPipeline(Pipeline pipeline, Jobs jobs, int *eof) {
-  int jobbed=0;
-  execute(pipeline,jobs,&jobbed,eof);
+extern void execPipeline(Pipeline pipeline, Jobs jobs, int *eof)
+{
+  int jobbed = 0;
+  execute(pipeline, jobs, &jobbed, eof);
   if (!jobbed)
-    freePipeline(pipeline);	// for fg builtins, and such
+    freePipeline(pipeline); // for fg builtins, and such
 }
 
-extern void freePipeline(Pipeline pipeline) {
-  PipelineRep r=(PipelineRep)pipeline;
-  deq_del(r->processes,freeCommand);
+extern void freePipeline(Pipeline pipeline)
+{
+  PipelineRep r = (PipelineRep)pipeline;
+  deq_del(r->processes, freeCommand);
   free(r);
+}
+
+extern int getlastprocess(Pipeline pipeline)
+{
+  PipelineRep r = (PipelineRep)pipeline;
+  return r->lastprocessid;
 }
