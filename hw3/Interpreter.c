3733adb55bb2c75f37d1735e39cdaaf6fa788a05
brianwu <dathamor@gmail.com>
2025-03-25 18:53:58 -0600

    pipelines and background tasks implemented

--- hw3/Interpreter.c
+++ hw3/Interpreter.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "Interpreter.h"
 #include "Sequence.h"
 #include "Pipeline.h"
 #include "Command.h"
+#include "deq.h"
 
 static Command i_command(T_command t);
 static void i_pipeline(T_pipeline t, Pipeline pipeline);
@@ -25,15 +27,30 @@ static void i_pipeline(T_pipeline t, Pipeline pipeline)
 {
   if (!t)
     return;
-  addPipeline(pipeline, i_command(t->command));
+  Command current = i_command(t->command);
+  addPipeline(pipeline, current);
   i_pipeline(t->pipeline, pipeline);
+
+  if (t->pipeline)
+  {
+    int pipefd[2];
+    pipe(pipefd);
+    // printf("%d %d %s\n", pipefd[0], pipefd[1], t->command->words->word->s);
+
+    Deq processes = getprocesses(pipeline);
+    Command previous = deq_tail_ith(processes, 0);
+
+    setwritefd(current, pipefd[1]);
+    setreadfd(previous, pipefd[0]);
+  }
 }
 
 static void i_sequence(T_sequence t, Sequence sequence)
 {
   if (!t)
     return;
-  Pipeline pipeline = newPipeline(1);
+  int fg = !(strcmp(t->op, "&") == 0);
+  Pipeline pipeline = newPipeline(fg);
   i_pipeline(t->pipeline, pipeline);
   addSequence(sequence, pipeline);
   i_sequence(t->sequence, sequence);
